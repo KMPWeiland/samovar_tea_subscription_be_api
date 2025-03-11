17093fcb8e9be8974627bd3573047d2a68981665
Kristin Weiland <kristinweiland@gmail.com>
2025-03-10 19:09:03 -0700

    Moved controllers to the correct namespace.  Finished index action, adding sorting functionality per FE spec requirement.

---
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -0,0 +1,19 @@
+class Api::V1::SubscriptionsController < ApplicationController
+
+  def index
+    subscriptions = Subscription.all
+
+    if params[:sorted].present? && params[:sorted] == "price"
+      subscriptions = Subscription.sort_by_price
+    elsif 
+      params[:sorted].present? && params[:sorted] == "status"
+      subscriptions = Subscription.sort_by_status
+    elsif 
+      params[:sorted].present? && params[:sorted] == "frequency"
+      subscriptions = Subscription.sort_by_frequency
+    end
+
+    render json: SubscriptionSerializer.new(subscriptions), status: :ok
+  end
+
+end
