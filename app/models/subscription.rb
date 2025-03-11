36c090ae29650d6b761fd6370be8568e0e9f358c
Kristin Weiland <kristinweiland@gmail.com>
2025-03-10 17:33:07 -0700

    Update model validations.

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,9 +1,14 @@
 class Subscription < ApplicationRecord
   validates :title, presence: true
   validates :price, presence: true
-  validates :status, presence: true
-  validates :frequency, presence: true
- 
+  validates :status, inclusion: { in: ["Active", "Cancelled", "Non-Payment"] }
+  validates :frequency, presence: true 
+
+  belongs_to :customer
+
   has_many :subscription_teas
   has_many :teas, through: :subscription_teas
 end
+
+
+
