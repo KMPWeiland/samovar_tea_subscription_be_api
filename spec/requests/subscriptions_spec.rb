59f64b6d58dea6220b063e58698f65f3630ea633
Kristin Weiland <kristinweiland@gmail.com>
2025-03-10 17:34:21 -0700

    Start Subscription request spec. Add SubscriptionSerializer

--- spec/requests/subscriptions_spec.rb
+++ spec/requests/subscriptions_spec.rb
@@ -1,7 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe "Subscriptions", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  let(:customer) { create(:customer) } 
+  let(:teas) { create_list(:tea, 3) }
+  let!(:subscriptions) { create_list(:subscription, 3, customer: customer) }
+
+
+  describe "Happy Paths" do
+    it "can retrieve a list of all subscriptions" do
+      # GET /index
+      get "/api/v1/subscriptions"
+
+      expect(response).to be_successful 
+      expect(response.status).to eq(200)
+    end
   end
+  
 end
