bd5497a6a34ab0b81b6021b58add1e3246f60ac7
Kristin Weiland <kristinweiland@gmail.com>
2025-03-10 17:32:43 -0700

    Create factories.

---
+++ spec/factories/subscription_factory.rb
@@ -0,0 +1,19 @@
+FactoryBot.define do
+  factory :subscription do
+    title { Faker::Subscription.plan }
+    price { Faker::Commerce.price(range: 14..100.0).to_d }
+    frequency { Faker::Subscription.subscription_term } 
+
+    trait :active do
+      status { 'Active' }
+    end
+
+    trait :cancelled do
+      status { 'Cancelled' }
+    end
+
+    trait :non_payment do
+      status { 'Non-Payment' }
+    end
+  end
+end
