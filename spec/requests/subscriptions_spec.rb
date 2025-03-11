93967214f1624e8f79af16f92f8725b2522855e5
Kristin Weiland <kristinweiland@gmail.com>
2025-03-10 19:58:58 -0700

    Add to subscriptions request spec.

--- spec/requests/subscriptions_spec.rb
+++ spec/requests/subscriptions_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 RSpec.describe "Subscriptions", type: :request do
 
-  let(:customer) { create(:customer) } 
-  let(:teas) { create_list(:tea, 3) }
-  let(:active_subscription) { create(:subscription, :active) }
-  let(:cancelled_subscription) { create(:subscription, :cancelled) }
-  let(:non_payment_subscription) { create(:subscription, :non_payment) }
+  let!(:customer) { create(:customer) } 
+  let!(:teas) { create_list(:tea, 3) }
+  let!(:active_subscription) { create(:subscription, :active, customer: customer, teas: teas) }
+  let!(:cancelled_subscription) { create(:subscription, :cancelled, customer: customer, teas: teas) }
+  let!(:non_payment_subscription) { create(:subscription, :non_payment, customer: customer, teas: teas) }
 
   # before(:each) do
   #   Subscription.destroy_all
@@ -27,22 +27,38 @@ RSpec.describe "Subscriptions", type: :request do
       json = JSON.parse(response.body, symbolize_names: true)
 
       expect(json[:data].first).to include(:id, :type, :attributes)
-      expect(json[:data].first[:attributes]).to include(:id, :title, :price, :status, :unit_price)
+      expect(json[:data].first[:attributes]).to include(:id, :title, :price, :status, :frequency)
     end
 
     it "should return a list of all subscriptions sorted by price when parameter is present" do
-      middle = create(:item, unit_price: 50)
-      cheap = create(:item, unit_price: 10)
-      expensive = create(:item, unit_price: 100)
 
-      get "/api/v1/items?sorted=price"
+      get "/api/v1/subscriptions?sorted=price"
       json = JSON.parse(response.body, symbolize_names: true)
 
       expect(response).to have_http_status(:ok)
-      expect(json[:data][0][:attributes][:name]).to eq(cheap.name)
-      expect(json[:data][1][:attributes][:name]).to eq(middle.name)
-      expect(json[:data][2][:attributes][:name]).to eq(expensive.name)
+
+      json = JSON.parse(response.body, symbolize_names: true)
+
+      expect(json[:data][0][:attributes][:price]).to be < json[:data][1][:attributes][:price]
+      expect(json[:data][1][:attributes][:price]).to be < json[:data][2][:attributes][:price]
     end
+
+    it "should return a single subscription by ID, including the tea(s) involved and what customer(s) is subscribed or has been in the past (if cancelled)" do
+
+      get `api/v1/subscriptions/#{active_subscription.id}`
+
+      expect(response).to have_http_status(:ok)
+
+      json = JSON.parse(response.body, symbolize_names: true)
+
+      binding.pry
+      
+      expect(json[:data]).to include(:id, :type, :attributes)
+      # expect(json[:data][:id]).to eq("#{item.id}")
+      # expect(json[:data][:type]).to eq("item")
+      # expect(json[:data][:attributes][:name]).to eq(name)
+      # expect(json[:data][:attributes][:description]).to eq(description)
+      # expect(json[:data][:attributes][:unit_price]).to eq(price)
+    end    
   end
-  
 end
